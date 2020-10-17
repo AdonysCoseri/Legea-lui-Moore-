1badcfb4802d55c1366abc32b1d963e232ae8c64
Adonys <adonys2710@gmail.com>
2020-10-17 23:04:29 +0300

    Legea lui Moore (Coseri  Adonys)

---
+++ Legea lui Moore/Legea lui Moore/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Legea_lui_Moore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,rez;
+            double  c = 0.11;
+            n = int.Parse(Console.ReadLine());
+            rez = (int)(n / c);
+            Console.WriteLine(rez);
+            Console.ReadKey();
+            
+
+        }
+    }
+}
